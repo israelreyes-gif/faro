26f416840496a405c0ccab7936158ed35d8c8357
israelreyes-gif <israel.reyes@gmail.com>
2026-08-08 01:59:51 +0200

    Update mensaje.js

--- js/mensaje.js
+++ js/mensaje.js
@@ -68,7 +68,6 @@ async function enviarMensaje() {
 }
 
 export function pintarMensajeDifundido(mensaje) {
-  document.getElementById('msg-name').textContent = mensaje.nombre;
   document.getElementById('msg-cat').textContent = mensaje.categoria.toUpperCase();
   document.getElementById('msg-text').textContent = mensaje.texto;
 }
